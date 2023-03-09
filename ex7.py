c76a8cc5c9ea302c5fdb6b5318bd96dbb70f720c
gilbu <gilbertbuix57@gmail.com>
2023-03-08 22:08:24 -0800

    add exercise 7

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# print a string
+print("Mary had a little lamb.")
+# format string then Print
+print("Its fleece was white as {}.".format('snow'))
+# print string
+print("And everywhere that Mary went.")
+# print "." 10 times
+print("." * 10) # what'd that do?
+
+# assign string to variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# concat end1 thru 6 and change what print does at the end from next line to a ' '
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# concat end7 to end12
+print(end7 + end8 + end9 + end10 + end11 + end12)
