304ceb5bd35b48d3154a90c70151d00ff07b6940
gilbu <gilbertbuix57@gmail.com>
2023-03-14 23:28:44 -0700

    add exercise 9

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# assign a string of days of the week to days variable
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# assign a string of months with a next line separator to months variables
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# print a sting and days variable containting a string
+print("here are the  days: ", days)
+# print a string and months variable containting a string
+print("here are the months: ", months)
+
+# print a string just as it's formatted
+print("""
+There's somthing going on here.
+With the three double-quots.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
