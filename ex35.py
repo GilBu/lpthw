9cab52a3f10c0945bbcd70b84800b124ce2dcfbb
gilbu <gilbertbuix57@gmail.com>
2023-06-26 22:25:57 -0700

    fix check for integer input

--- ex35.py
+++ ex35.py
@@ -4,9 +4,10 @@ def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice  = input("> ")
-    if "0" in choice or "1" in choice:
+
+    try:
         how_much = int(choice)
-    else:
+    except ValueError:
         dead("Man, learn to type a number.")
 
     if how_much < 50:
